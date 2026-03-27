9497fbd78a8921c2c3d4683d7d0668b58e69ed6b
guillermoanido <guilleanido2@gmail.com>
2026-03-27 17:40:51 -0300

    card data basic

--- ATC_Unity/Assets/Scripts/Core/Card.cs
+++ ATC_Unity/Assets/Scripts/Core/Card.cs
@@ -1,6 +1,36 @@
 using UnityEngine;
 
-public class NewEmptyCSharpScript
+public class Card : ScriptableObject 
 {
-    
+    public string cardName;
+    public CardType cardType;
+
+    public int energyCost;
+
+    public int speed;
+    public int damage;
+
+    public int strRequired;
+    public int intRequired;
+    public int wisRequired;
+    public int dexRequired;
+
+    public enum CardType
+    {
+        Accesory,
+        Armour,
+        Attack,
+        Aura,
+        Condition,
+        Consumable,
+        Equipment,
+        Shield,
+        Skill,
+        Spell,
+        Talent,
+        Weapon,
+    }
+
+
+
 }
