cf68b507d6086656f7c147f2de6ab8f991f7518c
guillermoanido <guilleanido2@gmail.com>
2026-03-30 15:42:12 -0300

    Hand Manager

---
+++ ATC_Unity/Assets/Scripts/UI/Hand Manager.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandManager : MonoBehaviour 
+{
+    public GameObject cardPrefab;
+    public Transform handPosition;
+    public float fanSpread = 5f;
+    public List<GameObject> cardsInHand = new List<GameObject>();
+
+    private void Start()
+    {
+        AddCardToHand();
+        AddCardToHand();
+        AddCardToHand();
+    }
+
+    public void AddCardToHand()
+    {
+        GameObject newCard = Instantiate(cardPrefab, handPosition.position, Quaternion.identity, handPosition);
+        cardsInHand.Add(newCard);
+
+        UpdateHandVisuals();
+    }
+
+    private void UpdateHandVisuals()
+    {
+        int cardCount = cardsInHand.Count;
+        for (int i = 0; i < cardCount; i++)
+        {
+            float rotationAngle = (fanSpread * (i - (cardCount-1) / 2f)) ;
+            cardsInHand[i].transform.localRotation = Quaternion.Euler(0f, 0f, rotationAngle);
+        }
+    }
+
+    private void Update()
+    {
+        
+    }
+
+
+}
