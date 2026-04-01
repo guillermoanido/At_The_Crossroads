d95f4b6f7b476e1e4a6988d53ac22891e6361950
guillermoanido <guilleanido2@gmail.com>
2026-04-01 13:09:06 -0300

    Deck

---
+++ ATC_Unity/Assets/Scripts/Core/Deck Manager.cs
@@ -0,0 +1,25 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeckManager : MonoBehaviour
+{
+    public List<Card> allCards = new List<Card>();
+
+    private int currentCardIndex = 0;
+
+    public void DrawCard(HandManager handManager)
+    {
+        if (allCards.Count == 0)
+        {
+            Debug.LogWarning("Deck is empty! Cannot draw a card.");
+            return;
+        }
+        Card nextCard = allCards[currentCardIndex];
+        handManager.AddCardToHand(nextCard);
+        currentCardIndex = (currentCardIndex +1) & allCards.Count;
+    }
+
+
+
+}
--- ATC_Unity/Assets/Scripts/UI/Hand Manager.cs
+++ ATC_Unity/Assets/Scripts/UI/Hand Manager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class HandManager : MonoBehaviour 
 {
+    public DeckManager deckManager;
+
     public GameObject cardPrefab;
     public Transform handPosition;
     
@@ -16,15 +18,20 @@ public class HandManager : MonoBehaviour
 
     private void Start()
     {
-        AddCardToHand();
-        AddCardToHand();
-        AddCardToHand();
+
     }
 
-    public void AddCardToHand()
+    void Update()
+    {
+        UpdateHandVisuals();
+    }
+
+    public void AddCardToHand(Card cardData)
     {
         GameObject newCard = Instantiate(cardPrefab, handPosition.position, Quaternion.identity, handPosition);
         cardsInHand.Add(newCard);
+
+        newCard.GetComponent<CardDisplay>().cardData = cardData;
 
         UpdateHandVisuals();
     }
@@ -32,21 +39,28 @@ public class HandManager : MonoBehaviour
     private void UpdateHandVisuals()
     {
         int cardCount = cardsInHand.Count;
+        if (cardCount == 1)
+        {
+            cardsInHand[0].transform.localRotation = Quaternion.Euler(0f, 0f, 0f);
+            cardsInHand[0].transform.localPosition = new Vector3(0f, 0f, 0f);
+            return;
+        }
+
         for (int i = 0; i < cardCount; i++)
         {
             float rotationAngle = (fanSpread * (i - (cardCount-1) / 2f));
             cardsInHand[i].transform.localRotation = Quaternion.Euler(0f, 0f, rotationAngle);
 
             float horizontalOffset = (cardSpacing * (i - (cardCount - 1) / 2f));
-            float verticalOffset = (verticalSpacing * (i - (cardCount - 1) / 2f));
+
+            float normalizedPosition = (2f * i /(cardCount - 1) - 1f);  
+            float verticalOffset = verticalSpacing * (1 - normalizedPosition * normalizedPosition);
+
+
             cardsInHand[i].transform.localPosition = new Vector3(horizontalOffset, verticalOffset, 0f);
         }
     }
 
-    private void Update()
-    {
-        UpdateHandVisuals();
-    }
 
 
 }
