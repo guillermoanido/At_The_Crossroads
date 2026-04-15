c5bbde274bd5859b60c2b7f9a85d5745fbe73443
guillermoanido <guilleanido2@gmail.com>
2026-04-15 17:24:02 -0300

    Update Card movement

--- ATC_Unity/Assets/Scripts/Core/Card Movement.cs
+++ ATC_Unity/Assets/Scripts/Core/Card Movement.cs
@@ -1,8 +1,123 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using System.Collections;
 using System.Collections.Generic;
 
 public class CardMovement : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerEnterHandler, IPointerExitHandler
 {
-    private 
+    private RectTransform rectTransform;
+
+    private Canvas canvas;
+
+    private Vector2 originalLocalPointerPosition;
+
+    private Vector3 originalPanelLocalPosition;
+
+    private Vector3 originalScale;
+
+    private int currentState;
+
+    private Quaternion originalRotation;
+    
+    private Vector3 originalPosition;
+
+    [SerializeField] private float selectScale = 1.1f;
+    [SerializeField] private Vector2 cardPlay;
+    [SerializeField] private Vector3 playPosition;
+    [SerializeField] private GameObject glowEffect;
+    [SerializeField] private GameObject playArrow;
+
+    void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+        canvas = GetComponent<Canvas>(); // inparent
+        originalScale = rectTransform.localScale;
+        originalRotation = rectTransform.localRotation;
+        originalPosition = rectTransform.localPosition;
+    }
+
+    void Update()
+    {
+        switch (currentState)
+        {
+            case 1: 
+                HandleHoverState();
+                break;
+            case 2:
+                HandleDragState();
+                if (!Input.GetMouseButtonDown(0)) // mouse release
+                {
+                    TransitionToState0 ();
+                }
+                break;
+            case 3:
+                HandlePlayState();
+                if (!Input.GetMouseButtonDown(0))
+                {
+                    TransitionToState0();
+                }
+                break;
+        }
+    }
+
+    private void TransitionToState0()
+    {
+        currentState = 0;
+        rectTransform.localScale = originalScale;
+        rectTransform.localRotation = originalRotation;
+        rectTransform.localPosition = originalPosition;
+        glowEffect.SetActive(false);
+        playArrow.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (currentState == 0)
+        {
+            originalPosition = rectTransform.localPosition;
+            originalRotation = rectTransform.localRotation;
+            originalScale = rectTransform.localScale;
+
+            currentState = 1;
+        }
+    }
+
+    public void OnPointerExit (PointerEventData eventData)
+    {
+      if (currentState == 1)
+        {
+            TransitionToState0();
+        }
+    }
+
+    public void OnPointerDown (PointerEventData eventData)
+    {
+        if (currentState == 1)
+        {
+            currentState = 2;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out originalLocalPointerPosition);
+            originalPanelLocalPosition = rectTransform.localPosition;
+        }
+    }
+
+    public void OnDrag (PointerEventData eventData)
+    {
+        if (currentState == 2)
+        {
+            if (RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.GetComponent<RectTransform>(), eventData.position, eventData.pressEventCamera, out Vector2 localPointerPosition))
+            {
+                localPointerPosition /= canvas.scaleFactor;
+
+                Vector3 offsetToOriginal = localPointerPosition - originalLocalPointerPosition;
+                rectTransform.localPosition = originalPanelLocalPosition + offsetToOriginal;
+            }
+        }
+    }
+
+    private void HandleHoverState()
+    {
+        glowEffect.SetActive(true);
+        rectTransform.localScale = originalScale * selectScale;
+        rectTransform.localRotation = Quaternion.Lerp(rectTransform.localRotation, Quaternion.Euler(0, 0, 0), Time.deltaTime * 10f);
+    }   
 }
